f9d1ca14cec2c063f82468ae24eb8fdbc3ed8b0d
Pauloh349 <muiruripaul3492@gmail.com>
2026-05-16 13:07:07 +0300

    update

--- script.js
+++ script.js
@@ -108,13 +108,13 @@ const featuredProjects = [
     "project-name": "Reclaima",
     "project-description": "Reconnecting you with your lost items",
     "project-tech-tags": [
-      "Python",
-      "Django",
-      "Cloudinary",
+      "ReactJS",
+      "NodeJS",
+      "SSE",
       "Render",
-      "PostgreSQL",
+      "MongoDB",
     ],
-    "try-it-link": "https://reclaima.onrender.com/",
+    "try-it-link": "http://reclaima-final-year-project.vercel.app/",
   },
 ];
 
