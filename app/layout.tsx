54d257b7ed523bc52526dd62fdf1653043061967
Pauloh349 <muiruripaul3492@gmail.com>
2026-08-24 11:34:19 +0300

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,6 +129,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png"/>
+        <link rel="manifest" href="favicon_io/site.webmanifest"/>
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
